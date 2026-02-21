5f1e60a02ba09a2f223cc5e4efea6bbf94349b2c
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-21 12:37:23 +0530

    Tweaked, tiles for better view

--- src/molecule/trendtile/TrendTile.jsx
+++ src/molecule/trendtile/TrendTile.jsx
@@ -1,12 +1,14 @@
 import './trendtile.css';
 
-import TileHeader from '../../atom/tileHeader/TileHeader';
 import DataTile from '../datatile/DataTile';
 
-function TrendTile(props){
+function TrendTile(props) {
     return <div className='trends-data'>
-        <TileHeader header = {props.header}></TileHeader>
-        <DataTile dailyAverage={props.dailyAverage} change={props.change} direction={props.direction} message={props.message}></DataTile>
+        <h3>{props.header}</h3>
+        <div className='trendsdata-card'>
+            <DataTile dailyAverage={props.dailyAverage} days={props.days} change={props.change} direction={props.direction} message={props.message}></DataTile>
+            <DataTile dailyAverage={props.dailyAverage} days={props.days} change={props.change} direction={props.direction} message={props.message}></DataTile>
+        </div>
     </div>
 }
 
