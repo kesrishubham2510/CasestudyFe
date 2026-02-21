cde6a056d0cd0cb129cd9914c6d8f1bfab63f2bb
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-21 15:06:23 +0530

    Introduced app context

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,35 @@
+//  to store the central state, like country for which the page is rendered
+
+import { createContext, useState } from "react";
+
+const appStateInit = {
+    'currCountry': 'India',
+    'country1': '',
+    'country2': '',
+    'country3': '',
+    'country4': '',
+    'offlineMode':false,
+    // The date of first covid-19 case report
+    'beginningDate': '2020-01-21',
+    'dateFormat': 'dd-MM-yyyy',
+    'dashboardTitle': 'Covistat'
+}
+
+
+const AppContext = createContext();
+
+const AppContextProvider = ({children}) => {
+    
+    const [state, setstate]  = useState(appStateInit);
+
+    return <AppContext.Provider value={{state, setstate}}>
+        {children}
+    </AppContext.Provider>
+}
+
+// consumable
+export {AppContext};
+
+// provider
+export default AppContextProvider;
+
