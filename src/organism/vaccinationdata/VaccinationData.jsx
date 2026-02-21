b7d0824037cfe1028704265db5ea3fc9906b2e5c
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-21 15:06:33 +0530

    Using context

--- src/organism/vaccinationdata/VaccinationData.jsx
+++ src/organism/vaccinationdata/VaccinationData.jsx
@@ -1,13 +1,17 @@
 import './vaccinationdata.css'
-
+import { useContext } from 'react';
 import VaccinationDose from "../../molecule/vaccinationdose/VaccinationDose"
+import { AppContext } from '../../context/AppContext';
 
 function VaccinationData() {
+
+    const appContext = useContext(AppContext);
+
     return <section className='vaccinationdose'>
         <h3>Vaccination Date</h3>
         <div className='stat-cards'>
-            <VaccinationDose text="Doses administered in India" number='23456774567'></VaccinationDose>
-            <VaccinationDose text="Doses administered in India" number='23456774567'></VaccinationDose>
+            <VaccinationDose text={`Doses administered in ${appContext.state.currCountry}`} number='23456774567'></VaccinationDose>
+            <VaccinationDose text="Doses administered Globally" number='23456774567'></VaccinationDose>
         </div>
     </section>
 }
--- src/page/Stats.jsx
+++ src/page/Stats.jsx
@@ -2,10 +2,17 @@ import './stats.css';
 import VaccinationData from '../organism/vaccinationdata/VaccinationData'
 import LatestStats from '../organism/lateststats/LatestStats';
 import Trends from '../organism/trends/Trends';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 function Stats() {
 
+    const appContext = useContext(AppContext);
+
+    console.log(appContext.state.currCountry);
+
     return <div className='stats'>
+        <h1>Covid-19 update for {appContext.state.currCountry}</h1>
         <LatestStats></LatestStats>
         <VaccinationData></VaccinationData>
         <Trends></Trends>
