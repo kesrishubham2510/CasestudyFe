aa23aa0629944f2ed35f843fbc663d102bf51f83
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-21 16:27:28 +0530

    Added logic to validate the country name

--- src/page/welcome/Welcome.jsx
+++ src/page/welcome/Welcome.jsx
@@ -3,11 +3,216 @@ import { useContext } from "react";
 
 import './welcome.css';
 import { AppContext } from "../../context/AppContext";
+import Toast from "../../molecule/toast/Toast";
 
 function Welcome() {
 
 
     var appContext = useContext(AppContext);
+
+    const countries = [
+        "Afghanistan",
+        "Albania",
+        "Algeria",
+        "Andorra",
+        "Angola",
+        "Antarctica",
+        "Antigua and Barbuda",
+        "Argentina",
+        "Armenia",
+        "Australia",
+        "Austria",
+        "Azerbaijan",
+        "Bahamas",
+        "Bahrain",
+        "Bangladesh",
+        "Barbados",
+        "Belarus",
+        "Belgium",
+        "Belize",
+        "Benin",
+        "Bhutan",
+        "Bolivia",
+        "Bosnia",
+        "Botswana",
+        "Brazil",
+        "Brunei",
+        "Bulgaria",
+        "Burkina Faso",
+        "Burma",
+        "Burundi",
+        "Cabo Verde",
+        "Cambodia",
+        "Cameroon",
+        "Canada",
+        "Central African Republic",
+        "Chad",
+        "Chile",
+        "China",
+        "Colombia",
+        "Comoros",
+        "Congo",
+        "Costa Rica",
+        "Côte d'Ivoire",
+        "Croatia",
+        "Cuba",
+        "Cyprus",
+        "Czechia",
+        "Denmark",
+        "Diamond Princess",
+        "Djibouti",
+        "Dominica",
+        "Dominican Republic",
+        "DRC",
+        "Ecuador",
+        "Egypt",
+        "El Salvador",
+        "Equatorial Guinea",
+        "Eritrea",
+        "Estonia",
+        "Ethiopia",
+        "Fiji",
+        "Finland",
+        "France",
+        "Gabon",
+        "Gambia",
+        "Georgia",
+        "Germany",
+        "Ghana",
+        "Greece",
+        "Grenada",
+        "Guatemala",
+        "Guinea-Bissau",
+        "Guinea",
+        "Guyana",
+        "Haiti",
+        "Holy See",
+        "Honduras",
+        "Hungary",
+        "Iceland",
+        "India",
+        "Indonesia",
+        "Iran",
+        "Iraq",
+        "Ireland",
+        "Israel",
+        "Italy",
+        "Jamaica",
+        "Japan",
+        "Jordan",
+        "Kazakhstan",
+        "Kenya",
+        "Kiribati",
+        "Kosovo",
+        "Kuwait",
+        "Kyrgyzstan",
+        "Lao People's Democratic Republic",
+        "Latvia",
+        "Lebanon",
+        "Lesotho",
+        "Liberia",
+        "Libyan Arab Jamahiriya",
+        "Liechtenstein",
+        "Lithuania",
+        "Luxembourg",
+        "Macedonia",
+        "Madagascar",
+        "Malawi",
+        "Malaysia",
+        "Maldives",
+        "Mali",
+        "Malta",
+        "Marshall Islands",
+        "Mauritania",
+        "Mauritius",
+        "Mexico",
+        "Micronesia",
+        "Moldova",
+        "Monaco",
+        "Mongolia",
+        "Montenegro",
+        "Morocco",
+        "Mozambique",
+        "MS Zaandam",
+        "N. Korea",
+        "Namibia",
+        "Nauru",
+        "Nepal",
+        "Netherlands",
+        "New Zealand",
+        "Nicaragua",
+        "Niger",
+        "Nigeria",
+        "Norway",
+        "Oman",
+        "Pakistan",
+        "Palau",
+        "Panama",
+        "Papua New Guinea",
+        "Paraguay",
+        "Peru",
+        "Philippines",
+        "Poland",
+        "Portugal",
+        "Qatar",
+        "Romania",
+        "Russia",
+        "Rwanda",
+        "S. Korea",
+        "Saint Kitts and Nevis",
+        "Saint Lucia",
+        "Saint Vincent and the Grenadines",
+        "Samoa",
+        "San Marino",
+        "Sao Tome and Principe",
+        "Saudi Arabia",
+        "Senegal",
+        "Serbia",
+        "Seychelles",
+        "Sierra Leone",
+        "Singapore",
+        "Slovakia",
+        "Slovenia",
+        "Solomon Islands",
+        "Somalia",
+        "South Africa",
+        "South Sudan",
+        "Spain",
+        "Sri Lanka",
+        "Sudan",
+        "Summer Olympics 2020",
+        "Suriname",
+        "Swaziland",
+        "Sweden",
+        "Switzerland",
+        "Syrian Arab Republic",
+        "Taiwan",
+        "Tajikistan",
+        "Tanzania",
+        "Thailand",
+        "Timor-Leste",
+        "Togo",
+        "Tonga",
+        "Trinidad and Tobago",
+        "Tunisia",
+        "Turkey",
+        "Tuvalu",
+        "UAE",
+        "Uganda",
+        "UK",
+        "Ukraine",
+        "Uruguay",
+        "USA",
+        "Uzbekistan",
+        "Vanuatu",
+        "Venezuela",
+        "Vietnam",
+        "West Bank and Gaza",
+        "Winter Olympics 2022",
+        "Yemen",
+        "Zambia",
+        "Zimbabwe"
+    ]
 
     var countryFieldStateInitial = {
         'countryName': '',
@@ -25,20 +230,45 @@ function Welcome() {
 
 
         setcountryFieldState((prevState) => {
-           return {
+            return {
                 ...prevState,
                 [inputId]: value
             };
+
         });
 
+    }
+
+    function resetError(){
+         setcountryFieldState((prevState)=>{
+                return {
+                    ...prevState,
+                    'err': '',
+                    'countryName': ''
+                }
+            })
+    }
+
+    function validateCountryName(country){
+        if(countries.indexOf(country)==-1){
+            setcountryFieldState((prevState)=>{
+                return {
+                    ...prevState,
+                    'err': 'Invalid Country',
+                    'countryName': ''
+                }
+            })
+        }
     }
 
     function searchStats() {
         const formattedDate = formatDate(countryFieldState.referenceDate);
 
-        if (countryFieldState.countryName.trim === '' || countryFieldState.countryName === '') {
+        if (countryFieldState.countryName.trim() === '' || countryFieldState.countryName === '') {
             return;
         }
+
+        validateCountryName(countryFieldState.countryName);
 
         console.log(countryFieldState.countryName, formattedDate)
     }
@@ -56,6 +286,11 @@ function Welcome() {
             </form>
         </div>
         <button className="searchButton" onClick={searchStats}>Search</button>
+       { countryFieldState.err.trim()   .length === 0 ? <div></div> : <Toast
+          message={countryFieldState.err}
+          type={'error'}
+          onClose={resetError}
+        /> }
     </section>
 }
 
